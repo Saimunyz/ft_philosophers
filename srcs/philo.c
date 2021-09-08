abe32883fdbfbeff8f29f2b8e59529f5fef4bd93
swagstaf <raning@mail.ru>
2021-09-09 01:40:34 +0300

    fix dedlock with 1 fork

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: swagstaf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/15 18:23:14 by swagstaf          #+#    #+#             */
-/*   Updated: 2021/09/09 01:22:57 by swagstaf         ###   ########.fr       */
+/*   Updated: 2021/09/09 01:38:17 by swagstaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,9 @@ t_data	parse_argv(int argc, char **argv)
 
 void	ft_print(char *str, t_philo *p)
 {
-	//pthread_mutex_lock(p->print);
 	if (p->stop)
-	{
-		//pthread_mutex_unlock(p->print);
 		return ;
-	}
 	printf("%lu %d %s\n", ft_time() - p->start, p->pos, str);
-	//pthread_mutex_unlock(p->print);
 }
 
 void	ft_usleep(int	time)
@@ -59,7 +54,7 @@ void	*live(void *philo)
 	t_philo *p;
 
 	p = (t_philo *)philo;
-	while (1 && !p->stop)
+	while (1 && !p->stop && !(p->right == p->left))
 	{
 		pthread_mutex_lock(p->left);
 		pthread_mutex_lock(p->right);
